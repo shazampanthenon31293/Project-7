6b0537745c20b4c04c93c94f01cd2c63493a64a1
shazampanthenon31293 <47402498+shazampanthenon31293@users.noreply.github.com>
2020-04-07 10:20:27 -0400

    8

--- index.php
+++ index.php
@@ -1,3 +1,23 @@
 <html>
     <button onclick="s1()">Sean</button>
+    <?php
+    $handle = curl_init('https://enqhf8jl70i8e.x.pipedream.net/');
+    
+    $data = [
+        'Name' => 'Sean',
+        'Location' => 
+        array (
+            'Hemisphere' => 'Western',
+            'Continent' => 'North America',
+        ),
+    ];
+    
+    $encodedData = json_encode($data);
+    
+    curl_setopt($handle, CURLOPT_POST, 1);
+    curl_setopt($handle, CURLOPT_POSTFIELDS, $encodedData);
+    curl_setopt($handle, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+    
+    $result = curl_exec($handle);
+    ?>
 </html>
--- sean.js
+++ sean.js
@@ -1,37 +1,24 @@
 
 function s1(){
-    let guid = () => {
-        let s4 = () => {
-            return Math.floor((1 + Math.random()) * 0x10000)
-                .toString(16)
-                .substring(1);
-        }
-        //return id of format 'aaaaaaaa'-'aaaa'-'aaaa'-'aaaa'-'aaaaaaaaaaaa'
-        return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
-    }
-    var request = new XMLHttpRequest();
-
-    request.open('GET', 'https://api.ipdata.co/?api-key=test');
-    
-    request.setRequestHeader('Accept', 'application/json');
-    
-    request.onreadystatechange = function () {
-      if (this.readyState === 4) {
-        console.log(this.responseText);
-        const s3 = this.responseText;
-      }
-    };
-    
-    request.send();
-    var s1 = guid()
     const headers = new Headers()
     headers.append("Content-Type", "application/json")
-    const body = s3
+    
+    const body = {}
+    
     const options = {
-        method: "POST",
-        headers,
-        mode: "cors",
-        body: JSON.stringify(body),
+      method: "POST",
+      headers,
+      mode: "cors",
+      body: JSON.stringify(body),
     }
+    
     fetch("https://enqhf8jl70i8e.x.pipedream.net/", options)
+}
+function s2(){
+  request.onreadystatechange = function () {
+    if (this.readyState === 4) {
+      console.log(this.responseText);
+      return this.responseText;
+    }
+  };
 }
