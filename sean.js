52be690cab995fc63f9bf37b78134f027886549d
shazampanthenon31293 <47402498+shazampanthenon31293@users.noreply.github.com>
2020-04-07 12:12:44 -0400

    6

--- sean.js
+++ sean.js
@@ -1,4 +1,4 @@
-
+var data = ""
 function s1(){
   var song = ""
   var Album = ""
@@ -16,8 +16,17 @@ function s1(){
     Artist = "Green Day"
     tracknumber = "4"
     Genre = "Alternative/Indie"
+  }else if(strUser=="American Idiot"){
+    document.getElementById("my-audio").setAttribute('src', '01 American Idiot.mp3');
+    var x = document.getElementById("my-audio");
+    x.play();
+    song="American Idiot"
+    Album= "American Idiot"
+    Artist = "Green Day"
+    tracknumber = "1"
+    Genre = "Alternative/Indie"
   }
-    const headers = new Headers()
+  const headers = new Headers()
     headers.append("Content-Type", "application/json")
     var s34 = document.getElementById("user_countrycode").innerHTML
     var ipadress = document.getElementById("user_ip").innerHTML
@@ -39,14 +48,24 @@ function s1(){
       mode: "cors",
       body: JSON.stringify(body),
     }
-    
     fetch("https://enqhf8jl70i8e.x.pipedream.net/", options)
-}
-function s2(){
+  }
+  function s2(){
+    var request = new XMLHttpRequest();
+  request.open('GET', 'https://api.ipdata.co/?api-key=test');
+  request.setRequestHeader('Accept', 'application/json');
   request.onreadystatechange = function () {
     if (this.readyState === 4) {
       console.log(this.responseText);
-      return this.responseText;
+      const headers1 = new Headers()
+      const options1 = {
+        method: "POST",
+        headers1,
+        mode: "cors",
+        body: this.responseText,
+      }
+      fetch("https://enqhf8jl70i8e.x.pipedream.net/",options1)
     }
-  };
+  }
+  request.send()
 }
